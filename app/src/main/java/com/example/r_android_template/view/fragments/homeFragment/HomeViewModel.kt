c87e5f3968ae65396bd252fbab877d06c148b082
Vango121 <barski93@gmail.com>
2021-11-05 15:36:23 +0100

    connect repository to viewmodel

--- app/src/main/java/com/example/r_android_template/view/fragments/homeFragment/HomeViewModel.kt
+++ app/src/main/java/com/example/r_android_template/view/fragments/homeFragment/HomeViewModel.kt
@@ -1,10 +1,42 @@
 package com.example.r_android_template.view.fragments.homeFragment
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.example.r_android_template.data.Repository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(): ViewModel() {
+class HomeViewModel @Inject constructor(private val repository: Repository) : ViewModel() {
 
+    private var _idLiveData: MutableLiveData<Int> = MutableLiveData()
+    val idLiveData: LiveData<Int>
+        get() = _idLiveData
+
+    init {
+        getID()
+    }
+    @DelicateCoroutinesApi
+    fun saveID(id: Int) {
+        GlobalScope.launch(Dispatchers.IO) {
+            repository.saveId(id)
+        }
+    }
+
+    @DelicateCoroutinesApi
+    fun getID() {
+        GlobalScope.launch(Dispatchers.IO) {
+            repository.getID().catch { e->
+                e.printStackTrace()
+            }.collect { _idLiveData.postValue(it) }
+        }
+    }
 }
