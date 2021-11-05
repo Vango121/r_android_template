a411d59dd8e2c604ac50d80fd5bd00c7bf8faf56
Vango121 <barski93@gmail.com>
2021-11-05 15:10:11 +0100

    update strings.xml

---
+++ app/src/main/java/com/example/r_android_template/di/AppModule.kt
@@ -0,0 +1,10 @@
+package com.example.r_android_template.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+}
