c8868a0a67bc4c41ac71f0865ffabef02093aa8f
Vango121 <barski93@gmail.com>
2021-11-05 11:52:23 +0100

    Create fragment and nav_host and add it to main_activity

---
+++ app/src/main/java/com/example/r_android_template/view/fragments/homeFragment/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.r_android_template.view.fragments.homeFragment
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
